42cd762c227934cdef6b7d9593185c9e46ce3b11
shazalia <shazaliawd@gmail.com>
2019-11-10 13:01:54 +0300

    splash module

---
+++ lib/pages/SignUpPage.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget{
+  @override
+  _signUpPageState createState() =>_signUpPageState() ;
+
+}
+
+ class _signUpPageState extends State<SignUpPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
